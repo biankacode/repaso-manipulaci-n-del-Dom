acef8ca3b10ed45e04a7413faab481b07f2603c7
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-01-21 19:30:48 -0600

    Se creo una funccion con if y else

---
+++ funciones.js
@@ -0,0 +1,25 @@
+const Suscripcion = "Expert"
+
+function suscripciones (tipoDeSuscripcion) {
+   if  ( tipoDeSuscripcion == "Free"){
+    return console.log("Solo puedes tomar los cursos gratis");
+    
+   }
+    else if (tipoDeSuscripcion == "Basic"){
+    return console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+    
+   } 
+   else if (tipoDeSuscripcion == "Expert"){
+    return console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+    
+   } 
+   else if  (tipoDeSuscripcion == "ExpertPlusree"){
+   return console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+       
+   }   
+} 
+
+
+const res = suscripciones(Suscripcion)
+
+
