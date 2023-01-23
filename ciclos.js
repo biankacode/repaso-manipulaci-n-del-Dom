e173528e07f22e0e0bf3156a958e57f0736b502e
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-01-23 10:05:27 -0600

    Se transformo el ciclo for a un ciclo while

---
+++ ciclos.js
@@ -0,0 +1,21 @@
+for (let i = 0; i < 5; i++){
+    console.log("El valor de i es:" + i);
+}
+/*[Running] node "/Users/bianka/Desktop/java Juan DC/tempCodeRunnerFile.js"
+El valor de i es:0
+El valor de i es:1
+El valor de i es:2
+El valor de i es:3
+El valor de i es:4*/
+let i = 0 
+
+while (i < 5) {
+    console.log("El valor de i es:" + i);
+    i++;
+}
+/*[Running] node "/Users/bianka/Desktop/java Juan DC/tempCodeRunnerFile.js"
+El valor de i es:0
+El valor de i es:1
+El valor de i es:2
+El valor de i es:3
+El valor de i es:4*/
