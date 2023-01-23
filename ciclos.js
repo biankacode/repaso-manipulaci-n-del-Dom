20d1820607b67410c090ba659396828c8d59d5ec
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-01-23 10:32:34 -0600

    Se creo un ciclo for con valo de 10 en disminucion y se sustituyo por un while

--- ciclos.js
+++ ciclos.js
@@ -19,3 +19,25 @@ El valor de i es:1
 El valor de i es:2
 El valor de i es:3
 El valor de i es:4*/
+
+for (let i = 10; i >= 2; i--){
+    console.log("El valor de i es : " + i);
+}
+/*[Running] node "/Users/bianka/Desktop/java Juan DC/tempCodeRunnerFile.js"
+El valor de i es : 10
+El valor de i es : 9
+El valor de i es : 8
+El valor de i es : 7
+El valor de i es : 6
+El valor de i es : 5
+El valor de i es : 4
+El valor de i es : 3
+El valor de i es : 2
+*/
+
+let a = 10;
+while (a >= 2) {
+    console.log("El valor de i es: " + a);
+    a --;
+    
+}
